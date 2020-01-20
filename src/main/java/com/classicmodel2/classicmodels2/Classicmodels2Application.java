03998f4ac09bc38083fdd3c2e990302fa7689580
Ramon Valles Ramos <ramon.valles@autozone.com>
2020-01-20 14:09:01 -0800

    Primer commit

---
+++ src/main/java/com/classicmodel2/classicmodels2/Classicmodels2Application.java
@@ -0,0 +1,13 @@
+package com.classicmodel2.classicmodels2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classicmodels2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Classicmodels2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/classicmodel2/classicmodels2/Classicmodels2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classicmodel2.classicmodels2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Classicmodels2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
