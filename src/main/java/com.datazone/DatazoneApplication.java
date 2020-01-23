115e482200d7e9bdaa06c6043c32754e6b905027
Ramon Valles Ramos <ramon.valles@autozone.com>
2020-01-23 15:12:54 -0800

    Add DatazoneApplication.java

--- src/main/java/com.datazone/DatazoneApplication.java
+++ src/main/java/com.datazone/DatazoneApplication.java
@@ -1,7 +1,18 @@
 package com.datazone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by autozoner on 1/23/2020.
  */
+@SpringBootApplication
 public class DatazoneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatazoneApplication.class, args);
+    }
 }
+
+
+
+
+
