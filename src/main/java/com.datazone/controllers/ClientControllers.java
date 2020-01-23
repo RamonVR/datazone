4f789376a0536943917374f26cb5fae3a0f5d25d
Ramon Valles Ramos <ramon.valles@autozone.com>
2020-01-23 14:35:23 -0800

    2020-01-23 Agregar ClientControllers

---
+++ src/main/java/com.datazone/controllers/ClientControllers.java
@@ -0,0 +1,7 @@
+package com.datazone.controllers;
+
+/**
+ * Created by autozoner on 1/23/2020.
+ */
+public class ClientControllers {
+}
---
+++ src/main/java/com.datazone/utility/UrlConstants.java
@@ -0,0 +1,15 @@
+package com.datazone.utility;
+
+/**
+ * Created by autozoner on 1/23/2020.
+ */
+public class UrlConstants {
+    public static final String INDEX = "";
+
+    public static final String CLIENTE = "/cliente";
+    public static final String CLIENTE_AGREGAR= "/agregar";
+    public static final String CLIENTE_ACTUALIZAR = "/actualizar";
+    public static final String CLIENTE_ELIMINAR = "/eliminar";
+    public static final String CLIENTE_LISTAR = "/listar";
+
+}
